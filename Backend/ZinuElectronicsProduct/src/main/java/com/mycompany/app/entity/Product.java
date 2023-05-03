d3ef7c3ef83de25bdbda7dc5bc877d3b09da1d0e
zinujust <shariff.zainal7@gmail.com>
2023-05-02 19:19:17 -0700

    Added repository and controller class

--- Backend/ZinuElectronicsProduct/src/main/java/com/mycompany/app/entity/Product.java
+++ Backend/ZinuElectronicsProduct/src/main/java/com/mycompany/app/entity/Product.java
@@ -1,6 +1,9 @@
 package com.mycompany.app.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +16,13 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String name;
 	private String description;
 	private String sku;
 	private double price;
-	private Integer discountPercent;
 	private Integer modelId;
 	private Integer brandId;
 	private Integer inventoryId;
